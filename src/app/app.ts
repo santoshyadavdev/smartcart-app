452e00d5a9ada872fd32ec5f668d2f1f9ee115f9
Santosh Yadav <santosh@coderabbit.ai>
2026-09-01 15:33:35 +0200

    add non-nodejs server

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
+import { httpResource } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('smartcartapp');
+
+  data = httpResource(() => '/api/data');
 }
